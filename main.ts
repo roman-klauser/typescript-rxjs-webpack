8325805e801cd91d8c4ea2989c753dc418d5c75e
Roman Klauser <romanklauser@Romans-MBP-2.fritz.box>
2018-03-01 08:25:37 +0100

    bunny jumps back

--- main.ts
+++ main.ts
@@ -2,18 +2,31 @@ import {Observable} from "rxjs/Observable";
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/switchMapTo';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/scan';
 
 import Bunny from './src/Bunny';
 
+function isEven(n) {
+    return n % 2 == 0;
+}
+
 const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
 const bunny = new Bunny('#bunny');
 
 
 const start$ = Observable.fromEvent(startButton, 'click');
-const interval$ = Observable.interval(1000);
+const stop$ = Observable.fromEvent(stopButton, 'click');
+const intervalStopable$ = Observable.interval(1000).takeUntil(stop$);
 
-start$.switchMap(e => interval$)
- .subscribe(x => bunny.takeStepForwards(x));
+start$
+    .switchMapTo(intervalStopable$)
+    .scan(acc => ({step: ++acc.step}), {step: 0})
+    .subscribe(acc => {
+        isEven(Math.ceil(acc.step / 4)) ? bunny.takeStepBackwards() : bunny.takeStepForwards()
+    });
 
 /*
 start$.subscribe(e =>
--- src/Bunny.ts
+++ src/Bunny.ts
@@ -2,27 +2,38 @@ import {TweenMax} from 'gsap';
 import {Observable} from "rxjs/Observable";
 import 'rxjs/add/observable/fromEvent';
 
-function isEven(n) {
-    return n % 2 == 0;
-}
 
 export default class Bunny {
 
     innerBunny;
 
-    constructor(id: string) {
-        const scream = new Audio('/assets/scream.wav');
+    constructor(id: string, isAdult?: boolean) {
+        let scream = new Audio('/assets/camera-click.wav');
         this.innerBunny = document.querySelector(id);
+        if (isAdult) {
+            this.innerBunny.style.cursor = 'url("assets/aim.png"), auto';
+            scream = new Audio('/assets/scream.wav')
+        }
+
         Observable.fromEvent(this.innerBunny, 'click')
             .subscribe(e => {
-                TweenMax.to(this.innerBunny, 0.75, { rotation: "+=360"})
+                if (isAdult) {
+                    TweenMax.to(this.innerBunny, 0.75, {rotation: "+=360"})
+                }
                 scream.play();
             })
     }
 
-    takeStepForwards(x) {
-        const  step = isEven(x) ? 100 : 50;
-        TweenMax.to(this.innerBunny, 0.50, {left: '+=' + step + 'px'});
+    takeStepForwards() {
+        this.innerBunny.style.transform = 'scaleX(1)';
+        TweenMax.to(this.innerBunny, 0.50, {left: '+=100px'});
+        TweenMax.to(this.innerBunny, 0.25, {top: '-=40px'});
+        TweenMax.to(this.innerBunny, 0.25, {top: '+=40px', delay: 0.25})
+    }
+
+    takeStepBackwards() {
+        this.innerBunny.style.transform = 'scaleX(-1)';
+        TweenMax.to(this.innerBunny, 0.50, {left: '-=100px'});
         TweenMax.to(this.innerBunny, 0.25, {top: '-=40px'});
         TweenMax.to(this.innerBunny, 0.25, {top: '+=40px', delay: 0.25})
     }
